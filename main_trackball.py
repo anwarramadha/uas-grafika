52340ca3e911e8d47d5b9a00139f80505029ff6e
hanmajid <han.majid2004@gmail.com>
2017-05-07 16:35:34 +0700

    can build cubes without texture

--- main_trackball.py
+++ main_trackball.py
@@ -3,10 +3,10 @@
 # Distributed under the (new) BSD License.
 # -----------------------------------------------------------------------------
 import numpy as np
-from glumpy import app, gl, glm, gloo
+from glumpy import app, gl, glm, gloo, data as data_glumpy
 from glumpy.geometry import colorcube
 from glumpy.transforms import Trackball, Position
-
+from os.path import abspath
 
 vertex = """
 uniform vec4 u_color;
@@ -31,6 +31,42 @@ void main()
 window = app.Window(width=1024, height=1024,
                     color=(0.30, 0.30, 0.35, 1.00))
 
+CUBES = []
+VIO = []
+
+def init_all_cubes(data):
+    global window, CUBES, vertex, fragment
+
+    for x, y, height, width in data:
+        vertices, faces, outline = custom_cube(x, y, height, width)
+
+        cube = gloo.Program(vertex, fragment)
+        cube.bind(vertices)
+        cube['transform'] = Trackball(Position("position"))
+        window.attach(cube['transform'])
+        CUBES.append(cube)
+        VIO.append((vertices, faces, outline))
+
+def custom_cube(x, y, height, width):
+    vertices, faces, outline = colorcube()
+    for t in vertices['position']:
+        t[0] += x
+        # height
+        if t[2] == 1:
+            t[2] = height
+        # width
+        if t[1] == 1:
+            t[1] = width
+    print(vertices)
+    return vertices, faces, outline
+
+def color_all_cubes():
+    global CUBES, VIO
+    for index, cube in enumerate(CUBES):
+        cube['u_color'] = 1, 1, 1, 1
+        # cube['texture'] = data_glumpy.get(abspath("lena.jpg"))/255.
+        cube.draw(gl.GL_TRIANGLES, VIO[index][1])
+
 @window.event
 def on_draw(dt):
     window.clear()
@@ -39,34 +75,35 @@ def on_draw(dt):
     gl.glDisable(gl.GL_BLEND)
     gl.glEnable(gl.GL_DEPTH_TEST)
     gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
-    cube['u_color'] = 1, 1, 1, 1
-    cube.draw(gl.GL_TRIANGLES, faces)
+    color_all_cubes()
 
     # Outlined cube
-    gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
-    gl.glEnable(gl.GL_BLEND)
-    gl.glDepthMask(gl.GL_FALSE)
-    cube['u_color'] = 0, 0, 0, 1
-    cube.draw(gl.GL_LINES, outline)
-    gl.glDepthMask(gl.GL_TRUE)
-
+    # gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
+    # gl.glEnable(gl.GL_BLEND)
+    # gl.glDepthMask(gl.GL_FALSE)
+    # cube['u_color'] = 0, 0, 0, 1
+    # cube.draw(gl.GL_LINES, outline)
+    # cube2['u_color'] = 0, 0, 0, 1
+    # cube2.draw(gl.GL_LINES, outline)
+    # gl.glDepthMask(gl.GL_TRUE)
+@window.event
+def on_key_press(key, modifiers):
+    global phi, theta
+    if key == app.window.key.UP:
+        glm.translate(CUBES[0], 0, 0, 0.1)
 
 # Build cube data
-V, I, O = colorcube()
-vertices = V.view(gloo.VertexBuffer)
-faces    = I.view(gloo.IndexBuffer)
-outline  = O.view(gloo.IndexBuffer)
-
-cube = gloo.Program(vertex, fragment)
-cube.bind(vertices)
-cube['transform'] = Trackball(Position("position"))
-window.attach(cube['transform'])
+data = [
+    (-2, 0, 1, 2),
+    (1, 0, 2, 1)]
+init_all_cubes(data)
 
 # OpenGL initalization
 gl.glEnable(gl.GL_DEPTH_TEST)
 gl.glPolygonOffset(1, 1)
 gl.glEnable(gl.GL_LINE_SMOOTH)
 gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+
 
 # Run
 app.run()
